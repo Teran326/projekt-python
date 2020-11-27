d2eb499741b13ff1c02d475d59a1fc6835ddd2b0
Teran326 <swysog@gmail.com>
2020-11-27 14:44:53 +0100

    properties

--- 04-oop-intro/ukol.py
+++ 04-oop-intro/ukol.py
@@ -42,15 +42,25 @@ class Hello:
         print(f'{self.nmb}, {self.favorite_number}, {self.random_number()}')
 
 class Bye(Hello):
-    def __init__(self, car, book, nmb, boom, word):
+    def __init__(self, car, book, nmb, boom, word, x):
         super().__init__(car, book, nmb)
         self.boom = boom
         self.word = word
+        self.x = x
+
+    @property
+    def x(self):
+        return self.__x
+
+    @x.setter
+    def x(self, hodnota):
+        self.__x = hodnota
 
     def __str__(self):
-        return f'({self.car}, {self.book}, {self.nmb}, {self.boom}, {self.word})'
+        return f'({self.car}, {self.book}, {self.nmb}, {self.boom}, {self.word}, {self.__x})'
+
 vse = Hello('Mustang', 'Zaklinac', 3)
-vse2 = Bye('Kia', 'Krysar', 1, 'booom', Hello.word())
+vse2 = Bye('Kia', 'Krysar', 1, 'booom', Hello.word(), 'Bye')
 print(vse)
 print(vse2)
 vse.all()
